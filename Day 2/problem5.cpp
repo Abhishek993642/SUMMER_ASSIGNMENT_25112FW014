12294d970199abd233f99fb5034b8593d73ad07f
Abhishek993642 <ay5157533@gmail.com>
2026-06-06 10:20:18 +0530

    add new file

---
+++ Day 2/problem5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,rem,sum=0;
+    cout<<"enter the number";
+    cin>>n;
+    while(n!=0){
+        rem=n%10;
+        sum +=rem;
+        n=n/10;
+
+    }
+    cout<<sum;
+    return 0;
+}
